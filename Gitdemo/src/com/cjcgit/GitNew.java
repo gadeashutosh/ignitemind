80658b75075249c256fd6c71cdd4fbbb224fde1f
Admin <Admin@DESKTOP-A18EGLJ>
2023-03-03 22:52:44 +0530

    By Ashutosh1

---
+++ Gitdemo/src/com/cjcgit/GitNew.java
@@ -0,0 +1,16 @@
+package com.cjcgit;
+
+public class GitNew {
+
+public static void main(String[] args) {
+	
+System.out.println("Thanku Ankit");
+System.out.println("Thanku Ankit");
+System.out.println("Thanku Ankit");
+System.out.println("Thanku Ankit");
+
+
+
+		
+	}
+}
