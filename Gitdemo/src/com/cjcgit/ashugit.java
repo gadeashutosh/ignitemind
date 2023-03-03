cdc300917bd6dac7c7d08d3134924e3e157d830d
Admin <Admin@DESKTOP-A18EGLJ>
2023-03-03 22:46:56 +0530

    By Ashutosh

---
+++ Gitdemo/src/com/cjcgit/ashugit.java
@@ -0,0 +1,15 @@
+package com.cjcgit;
+
+public class ashugit {
+	public static void main(String[] args) {
+		
+		System.out.println("Ashutosh");
+		System.out.println("Ashutosh");
+		System.out.println("Ashutosh");
+
+		System.out.println("Ashutosh");
+
+
+	}
+
+}
