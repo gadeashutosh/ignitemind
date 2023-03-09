af5619cad1480305e25a87be3fae66015c819834
Admin <Admin@DESKTOP-A18EGLJ>
2023-03-09 16:08:19 +0530

    by Ashu

---
+++ GitJenkins/src/Demogits/Demo.java
@@ -0,0 +1,10 @@
+package Demogits;
+
+public class Demo {
+public static void main(String[] args)
+{
+	System.out.println("Ashutosh Gadhe");
+	System.out.println("Gits-jenkins integration");
+	
+}
+}
